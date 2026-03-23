d3824f3e26b94f84b50f9b0422d53500c7fbcecf
suzanasdm <164531292+suzanasdm@users.noreply.github.com>
2026-03-23 18:59:03 -0300

    suzana 23/03

---
+++ Integrador-Front/src/app/contabancaria/contabancaria.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contabancaria',
+  imports: [],
+  templateUrl: './contabancaria.html',
+  styleUrl: './contabancaria.css',
+})
+export class Contabancaria {}
